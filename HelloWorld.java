ca639c711597e18b4eacf27bb43438bb51b62d49
MasterofBait <127375665+onquit@users.noreply.github.com>
2024-03-06 17:02:51 +0800

    Update and rename myfirstjavafile.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
 
